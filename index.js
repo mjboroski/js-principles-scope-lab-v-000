4b8218bda27a4c2b83622219703f01a2b2932e38
Michael Boroski <mjboroski@gmail.com>
2018-07-08 01:29:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 var customerName = "bob";
+
 function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  return customerName = customerName.toUpperCase();
+}
+
+function overwriteBestCustomer(){
+  return customerName = customerName.toUpperCase();
+}
+
+function leastFavoriteCustomer (){
+  return customerName = customerName.toUpperCase();
+}
+
+function changeLeastFavoriteCustomer (){
   return customerName = customerName.toUpperCase();
 }
