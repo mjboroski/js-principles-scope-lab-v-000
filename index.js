ab1dba717f36cf1392b6755f9a3533552a297a35
Michael Boroski <mjboroski@gmail.com>
2018-07-08 01:33:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function overwriteBestCustomer(customer){
 const leastFavoriteCustomer = "steve";
 
 function changeLeastFavoriteCustomer (){
-  return leastFavoriteCustomer = "danny";
+  leastFavoriteCustomer = "danny";
 }
